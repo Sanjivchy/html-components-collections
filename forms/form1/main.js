81d48457503af63171f3c4349dfede029bb654be
xoonziip <sanjivchaudhary416@gmail.com>
2021-05-30 14:44:45 +0545

    form1 res

--- forms/form1/main.js
+++ forms/form1/main.js
@@ -1,6 +1,10 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const signbtnMbl = document.getElementById('signBtn');
+const signbtnMbl2 = document.getElementById('signBtn1');
+
+const formContainer = document.getElementById('formContainer')
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -9,3 +13,18 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+signbtnMbl.addEventListener("click", () => {
+	toggle();
+  });
+  signbtnMbl2.addEventListener("click", () => {
+	toggle();
+  });
+
+
+function toggle() {
+  formContainer.classList.toggle("sign-mbl");
+}
+
+  
